43f08ceabce4aa2c77107c5061ab9ea0228952c6
rconn <rconnors96@gmail.com>
2021-08-07 15:22:05 +0900

    testing simple game

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -63,24 +64,47 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render() {
 		ScreenUtils.clear(0, 0, 0.2f, 1);
 
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+
+		// begin a new batch and draw the bucket and
+		// all drops
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+		batch.end();
+
+		// process user input
 		if(Gdx.input.isTouched()) {
 			Vector3 touchPos = new Vector3();
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			bucket.x = (int) (touchPos.x - 64 / 2);
+			bucket.x = touchPos.x - 64 / 2;
 		}
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
 
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) {
-			spawnRaindrop();
-		}
+		// make sure the bucket stays within the screen bounds
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
 
+		// check if we need to create a new raindrop
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+		// move the raindrops, remove any that are beneath the bottom edge of
+		// the screen or that hit the bucket. In the latter case we play back
+		// a sound effect as well.
 		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
 			Rectangle raindrop = iter.next();
 			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if(raindrop.y + 64 < 0) {
-				iter.remove();
-			}
-			if (raindrop.overlaps(bucket)) {
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
 				dripSound.play();
 				iter.remove();
 			}
